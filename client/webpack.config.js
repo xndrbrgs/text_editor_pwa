065c9cafb708de676b0d4512b7bad0c3ec292069
xndrbrgs <aborgescolon@gmail.com>
2022-03-01 16:24:11 -0500

    merged from older project webpack config dependencies

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+				template: './index.html',
+			}),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+			new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: 'JATE',
+				short_name: 'JATE',
+				description: 'Just Another Text Editor',
+				display: 'standalone',
+				background_color: 'rgb(9,9,9)',
+				theme_color: 'rgb(9,9,9)',
+				start_url: '/',
+				publicPath: '/',
+				icons: [
+					{
+						src: path.resolve('src/images/logo.png'),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join('assets', 'icons'),
+					},
+				],
+			}),
     ],
 
     module: {
       rules: [
-        
+        {
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+        {
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+							plugins: [
+								'@babel/plugin-proposal-object-rest-spread',
+								'@babel/transform-runtime',
+							],
+						},
+					},
+				},
       ],
     },
   };
