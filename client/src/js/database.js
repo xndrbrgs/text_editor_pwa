bbc639a01a6634c4986a077501d3575fab6239cc
xndrbrgs <aborgescolon@gmail.com>
2022-03-01 16:10:06 -0500

    getDB on database completed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,19 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Gets the text back from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  return result?.value;
+} 
 
 initdb();
